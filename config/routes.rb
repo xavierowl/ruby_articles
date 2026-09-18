63994835058a4c31eefa90c6eb115f84c4d0dbd7
root <root@Byron-PC>
2026-09-18 08:29:58 -0500

    [ADD] Authentication for user added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root 'pages#landing_page'
+  devise_for :users
+
+  authenticated(:user) do
+    root "pages#index", as: :authenticated_root
+  end
+  unauthenticated(:user) do
+    root "pages#landing_page", as: :unauthenticated_root
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
         "evaluationLight": "#ddffd9",
         "chapterStrong": "#05636f",
         "surveyStrong": "#686f05",
+        "formBlack": "#1f2937"
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
